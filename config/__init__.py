1a51f81a171b430b968c0c77f0e00f2cedffb47d
netizen-0 <rishabhytpremium17@gmail.com>
2026-02-03 08:20:44 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -14,8 +14,8 @@ MONGO_DB = os.getenv("MONGO_DB", "oxygen")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "0"))
-SUPPORT_CHAT_URL = os.getenv("SUPPORT_CHAT_URL", "https://t.me/botsyard")
-DEVELOPER_URL = os.getenv("DEVELOPER_URL", "https://t.me/oxeign")
+SUPPORT_CHAT_URL = os.getenv("SUPPORT_CHAT_URL", "https://t.me/BotzEmpire")
+DEVELOPER_URL = os.getenv("DEVELOPER_URL", "https://t.me/btw_deva")
 PANEL_IMAGE_URL = os.getenv("PANEL_IMAGE_URL", "https://files.catbox.moe/uvqeln.jpg")
 
 _missing = [name for name, val in {"BOT_TOKEN": BOT_TOKEN, "API_ID": API_ID, "API_HASH": API_HASH}.items() if not val]
